4b8a7d5a34b8653f3d2fa1cdf321617cc3a2a0a1
Santi <santycar82@gmail.com>
2026-03-28 20:13:47 -0600

    :D

--- bases/src/app/app.ts
+++ bases/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Santi Rodriguez');
+  protected readonly title = signal('Santiago Rodriguez');
 }
