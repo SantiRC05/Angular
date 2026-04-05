52b546313e4fb14c34ad7ecb6171f272892d3fba
SantiRC05 <santycar82@gmail.com>
2026-04-04 22:11:45 -0600

    media solucion

--- bases/src/app/pages/counter/hero/hero-page.component.ts
+++ bases/src/app/pages/counter/hero/hero-page.component.ts
@@ -5,6 +5,26 @@ import { ChangeDetectionStrategy, Component, signal} from "@angular/core";
   templateUrl: `./hero-page.component.html`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+export class HeroPageComponent {
+  name: string = 'Spiderman';
+  age: number = 25;
 
-export class HeroPageComponent {}
+  get nameUpper(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Ironman';
+    this.age = 45;
+  }
+
+  resetForm(): void {
+    this.name = 'Spiderman';
+    this.age = 25;
+  }
+}
 
