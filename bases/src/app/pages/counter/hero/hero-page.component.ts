db27bdb4038919788264f34795d9bdf518b2f55c
SantiRC05 <santycar82@gmail.com>
2026-04-05 22:03:21 -0600

    señales

--- bases/src/app/pages/counter/hero/hero-page.component.ts
+++ bases/src/app/pages/counter/hero/hero-page.component.ts
@@ -1,30 +1,35 @@
-import { ChangeDetectionStrategy, Component, signal} from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
   selector: 'app-hero-page',
-  templateUrl: `./hero-page.component.html`,
+  templateUrl: './hero-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroPageComponent {
-  name: string = 'Spiderman';
-  age: number = 25;
+
+  name = signal<string>('Ironman');
+  age = signal<number>(45);
 
   get nameUpper(): string {
-    return this.name.toUpperCase();
+    return this.name().toUpperCase();
   }
 
   getHeroDescription(): string {
-    return `${this.name} - ${this.age}`;
+    return `${this.name()} - ${this.age()}`;
   }
 
   changeHero(): void {
-    this.name = 'Ironman';
-    this.age = 45;
+    this.name.set('Spiderman');
+    this.age.set(22);
   }
 
   resetForm(): void {
-    this.name = 'Spiderman';
-    this.age = 25;
+    this.name.set('Ironman');
+    this.age.set(45);
+  }
+
+  changeAge(): void {
+    this.age.set(60);
   }
 }
 
