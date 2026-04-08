d740dd7b2d9b27c9e2a347e84fe027ad5a00e294
SantiRC05 <santycar82@gmail.com>
2026-04-07 20:11:08 -0600

    solucion #1

--- bases/src/app/pages/counter/hero/hero-page.component.ts
+++ bases/src/app/pages/counter/hero/hero-page.component.ts
@@ -1,35 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class HeroPageComponent {
 
-  name = signal<string>('Ironman');
-  age = signal<number>(45);
-
-  get nameUpper(): string {
-    return this.name().toUpperCase();
-  }
-
-  getHeroDescription(): string {
-    return `${this.name()} - ${this.age()}`;
-  }
-
-  changeHero(): void {
-    this.name.set('Spiderman');
-    this.age.set(22);
-  }
-
-  resetForm(): void {
-    this.name.set('Ironman');
-    this.age.set(45);
-  }
-
-  changeAge(): void {
-    this.age.set(60);
-  }
 }
 
