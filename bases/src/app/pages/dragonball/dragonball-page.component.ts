19f6a8e83448b47ddb377a99966020db81e85bb2
SantiRC05 <santycar82@gmail.com>
2026-04-13 21:18:19 -0600

    @for

--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,7 +1,22 @@
 import { Component, computed, signal } from "@angular/core";
 
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+
 @Component({
   templateUrl: `./dragonball-page.component.html`
 })
 
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8001 },
+    { id: 3, name: 'Gohan', power: 3001 },
+  ]);
+
+
+}
