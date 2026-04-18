b4ae340b3390b59ac344d5249a97876c2b4925db
SantiRC05 <santycar82@gmail.com>
2026-04-17 22:36:01 -0600

    Agregar personajes

--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -29,4 +29,24 @@ export class DragonballPageComponent {
       'text-danger':true,
     };
   });
+
+
+  addCharacter() {
+    if( !this.name() || this.power() <= 0 )
+      return;
+
+  const newCharacter: Character = {
+    id:this.characters().length + 1,
+    name: this.name(),
+    power: this.power(),
+  };
+  this.characters.update( (list) => [...list, newCharacter] );
+  this.resetFields();
+
+  }
+resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  };
+
 }
