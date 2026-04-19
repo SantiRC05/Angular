35e6587fa3378d478064d07e8ed549f3201c43de
SantiRC05 <santycar82@gmail.com>
2026-04-18 21:37:04 -0600

    nueva seccion en nav

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CounterComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/counter/hero/hero-page.component';
 import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
+import { DragonballSuperPageComponent } from './pages/dragonball-super/dragonball-super-page.component';
 
 export const routes: Routes = [
 
@@ -22,8 +23,13 @@ export const routes: Routes = [
 },
 
 {
+  path: 'dragonball-super',
+  component: DragonballSuperPageComponent
+},
+
+{
   path: '**',
   redirectTo: ''
-}
+},
 
 ];
--- bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,5 +1,5 @@
-import { NgClass } from "@angular/common";
-import { Component, computed, signal } from "@angular/core";
+
+import { Component, signal } from "@angular/core";
 
 interface Character {
   id: number;
@@ -9,26 +9,19 @@ interface Character {
 
 
 @Component({
-  templateUrl: `./dragonball-page.component.html`,
+  templateUrl: `./dragonball-super-page.component.html`,
+  selector: 'dragonball-super',
   //imports: [NgClass]
 })
 
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
   name = signal('');
   power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    // { id: 2, name: 'Vegeta', power: 8001 },
-    // { id: 3, name: 'Gohan', power: 3001 },
-    // { id: 4, name: 'Yamcha', power: 500 },
+    { id: 2, name: 'Vegeta', power: 8001 },
   ]);
-
-  powerClasses = computed(() => {
-    return {
-      'text-danger':true,
-    };
-  });
 
 
   addCharacter() {
