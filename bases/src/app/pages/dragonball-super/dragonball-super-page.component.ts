dc20856b229066e62ea5eaa9eb0d3f025bf7d076
SantiRC05 <santycar82@gmail.com>
2026-04-27 21:08:23 -0600

    Services

--- bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,7 +1,8 @@
 
-import { Component, signal } from "@angular/core";
+import { Component, inject} from "@angular/core";
 import { CharacterList } from "../../components/dragonball/character-list/character-list";
 import { CharacterAdd } from "../dragonball/character-add/character-add";
+import { DragonBallService } from "../../services/dragonball.service";
 
 interface Character {
   id: number;
@@ -14,24 +15,8 @@ interface Character {
   templateUrl: `./dragonball-super-page.component.html`,
   selector: 'dragonball-super',
   imports: [CharacterList, CharacterAdd],
-  //imports: [NgClass]
 })
 
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-  addCharacter(newCharacter: Character) {
-    this.characters.update((list) => [...list, newCharacter]);
+  public dragonballService = inject(DragonBallService);
   }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
-}
